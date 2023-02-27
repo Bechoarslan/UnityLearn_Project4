50ce4f7db8b23820636bf3646ad82af39d5b3d33
Bechoarslan <barslann09@gmail.com>
2023-02-27 10:12:44 +0300

    UnityLearn_Project4

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PowerUpType currentPowerUp = PowerUpType.None;
+    public GameObject rocketPrefab;
+    private GameObject tmpRocket;
+    private Coroutine powerupCountdown;
+
+    public float playerSpeed = 5.0f;
+    private float awayEnemy = 15.0f;
+    private Rigidbody playerRb;
+    private GameObject focalPosition;
+    public bool powerUp;
+    public GameObject PowerUpSphere;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPosition = GameObject.Find("Focal Point");
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(playerSpeed * verticalInput * focalPosition.transform.forward);
+        PowerUpSphere.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            powerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpTimer());
+            PowerUpSphere.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            if (powerupCountdown != null)
+            {
+                StopCoroutine(powerupCountdown);
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
+        }
+
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        currentPowerUp = PowerUpType.None;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp ==
+PowerUpType.Pushback)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position -
+            transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength,
+            ForceMode.Impulse);
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with
+            powerup set to " + currentPowerUp.ToString());
+}
+
+        if (collision.gameObject.CompareTag("BigEnemy"))
+        {
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            playerRb.AddForce(Vector3.forward * 10, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpTimer()
+    {
+        yield return new WaitForSeconds(7);
+        powerUp = false;
+        PowerUpSphere.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPreb;
+    private float spawnArea = 9.0f;
+   private Vector3 spawnPoint;
+    public int enemyCounter;
+    public int spawnWave = 1;
+    public GameObject powerUpPrebafs;
+    private int levelDiffulicty = 0;
+    void Start()
+    {
+        SpawnEnemyLoop(spawnWave);
+        Instantiate(powerUpPrebafs, GenerateRandomPosition(), powerUpPrebafs.transform.rotation);
+    }
+
+
+    
+    void Update()
+    { 
+        
+
+        enemyCounter = FindObjectsOfType<Enemy>().Length;
+        if(enemyCounter == 0)
+        {
+            spawnWave++;
+            SpawnEnemyLoop(spawnWave);
+            Instantiate(powerUpPrebafs, GenerateRandomPosition(), powerUpPrebafs.transform.rotation);
+            levelDiffulicty++;
+            SpawnBigEnemy();
+        }
+
+        
+    }
+    void SpawnEnemyLoop(int enemiesSpawn)
+    {
+        for (int i = 0; i < enemiesSpawn; i++)
+        {
+
+            Instantiate(enemyPreb[0], GenerateRandomPosition(), enemyPreb[0].transform.rotation);
+            
+           
+        }
+       
+
+    }
+    void SpawnBigEnemy()
+    {
+        if(levelDiffulicty > 4)
+        {
+            Instantiate(enemyPreb[1], GenerateRandomPosition(), enemyPreb[1].transform.rotation);
+        }
+    }
+    private Vector3 GenerateRandomPosition()
+    {
+
+        float RandomX = Random.Range(-spawnArea, spawnArea);
+        float RandomZ = Random.Range(-spawnArea, spawnArea);
+
+        spawnPoint = new Vector3(RandomX, 0, RandomZ);
+        return spawnPoint;
+    }
+}
